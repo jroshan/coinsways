5673af4ac208f87f4c34b78de0331782096564c9
pramod-prasad <pramod.prasad@globrin.com>
2017-04-15 13:21:27 +0530

    Masters changes
    Masters changes

---
+++ Coinsways/Coinsways/Areas/Admin/Controllers/HelpDeskController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Coinsways.Entities;
+
+namespace Coinsways.Areas.Admin.Controllers
+{
+    public class HelpDeskController : Controller
+    {
+        private CoinswaysDbEntities db = new CoinswaysDbEntities();
+
+        // GET: Admin/HelpDesk
+        public async Task<ActionResult> Index()
+        {
+            var helpDeskQueries = db.HelpDeskQueries.Include(h => h.HelpType).Include(h => h.UserDetail);
+            return View(await helpDeskQueries.ToListAsync());
+        }
+
+        // GET: Admin/HelpDesk/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HelpDeskQuery helpDeskQuery = await db.HelpDeskQueries.FindAsync(id);
+            if (helpDeskQuery == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.HelpTypeId = new SelectList(db.HelpTypes, "ID", "Name", helpDeskQuery.HelpTypeId);
+            ViewBag.UserId = new SelectList(db.UserDetails, "UserId", "Name", helpDeskQuery.UserId);
+            return View(helpDeskQuery);
+        }
+
+        // POST: Admin/HelpDesk/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,UserId,HelpTypeId,Message,IsActive,CreatedDate")] HelpDeskQuery helpDeskQuery)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(helpDeskQuery).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.HelpTypeId = new SelectList(db.HelpTypes, "ID", "Name", helpDeskQuery.HelpTypeId);
+            ViewBag.UserId = new SelectList(db.UserDetails, "UserId", "Name", helpDeskQuery.UserId);
+            return View(helpDeskQuery);
+        }
+
+        // GET: Admin/HelpDesk/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HelpDeskQuery helpDeskQuery = await db.HelpDeskQueries.FindAsync(id);
+            if (helpDeskQuery == null)
+            {
+                return HttpNotFound();
+            }
+            return View(helpDeskQuery);
+        }
+
+        // POST: Admin/HelpDesk/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            HelpDeskQuery helpDeskQuery = await db.HelpDeskQueries.FindAsync(id);
+            db.HelpDeskQueries.Remove(helpDeskQuery);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Coinsways/Coinsways/Areas/Admin/Controllers/HelpTypeController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Coinsways.Entities;
+
+namespace Coinsways.Areas.Admin.Controllers
+{
+    public class HelpTypeController : Controller
+    {
+        private CoinswaysDbEntities db = new CoinswaysDbEntities();
+
+        // GET: Admin/HelpType
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.HelpTypes.ToListAsync());
+        }
+
+        // GET: Admin/HelpType/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/HelpType/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ID,Name,IsActive")] HelpType helpType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.HelpTypes.Add(helpType);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(helpType);
+        }
+
+        // GET: Admin/HelpType/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HelpType helpType = await db.HelpTypes.FindAsync(id);
+            if (helpType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(helpType);
+        }
+
+        // POST: Admin/HelpType/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ID,Name,IsActive")] HelpType helpType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(helpType).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(helpType);
+        }
+
+        // GET: Admin/HelpType/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HelpType helpType = await db.HelpTypes.FindAsync(id);
+            if (helpType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(helpType);
+        }
+
+        // POST: Admin/HelpType/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            HelpType helpType = await db.HelpTypes.FindAsync(id);
+            db.HelpTypes.Remove(helpType);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
